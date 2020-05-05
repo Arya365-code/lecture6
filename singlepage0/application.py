0c3c084b962477e899ba157ad6c9eb75cffe2e18
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-05 18:14:34 +1000

    Create application.py

---
+++ singlepage0/application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+texts = ["Sed velit ipsum, pellentesque vitae quam quis, elementum maximus lectus. Nulla a lorem dui. Curabitur efficitur sem in dui dignissim, eu blandit elit fermentum. Nulla in pulvinar magna. Cras aliquam tortor ut tempor sollicitudin. Maecenas in semper sem. Nullam lorem justo, venenatis ut diam eu, venenatis scelerisque sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed risus elit, sagittis sit amet enim ut, maximus posuere turpis. Nam et risus aliquet, bibendum magna eget, rutrum metus. Aliquam blandit tempor mi dictum sagittis. Sed porttitor aliquet finibus. Suspendisse commodo elit non sem varius congue."
+
+@app.route("/first")
+def first():
+  return texts[0]
+
+@app.route("/second")
+def second():
+  return texts[1]
+
+@app.route("/third")
+def third():
+  return texts[2]
