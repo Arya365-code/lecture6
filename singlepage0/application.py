3a175c87a4d62b5bd126c21bec27661b19d833f1
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-05 18:16:22 +1000

    Update application.py

--- singlepage0/application.py
+++ singlepage0/application.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-texts = ["Sed velit ipsum, pellentesque vitae quam quis, elementum maximus lectus. Nulla a lorem dui. Curabitur efficitur sem in dui dignissim, eu blandit elit fermentum. Nulla in pulvinar magna. Cras aliquam tortor ut tempor sollicitudin. Maecenas in semper sem. Nullam lorem justo, venenatis ut diam eu, venenatis scelerisque sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed risus elit, sagittis sit amet enim ut, maximus posuere turpis. Nam et risus aliquet, bibendum magna eget, rutrum metus. Aliquam blandit tempor mi dictum sagittis. Sed porttitor aliquet finibus. Suspendisse commodo elit non sem varius congue."
+texts = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempor, felis eget laoreet suscipit, dui libero ultricies enim, non pretium massa lectus vitae justo. Cras eu sapien tincidunt, gravida lectus id, ultricies elit. Phasellus fringilla arcu id congue semper. Phasellus et elit scelerisque, pellentesque lorem et, condimentum eros. ",
+         "Sed velit ipsum, pellentesque vitae quam quis, elementum maximus lectus. Nulla a lorem dui. Curabitur efficitur sem in dui dignissim, eu blandit elit fermentum. Nulla in pulvinar magna. Cras aliquam tortor ut tempor sollicitudin. Maecenas in semper sem. Nullam lorem justo, venenatis ut diam eu, venenatis scelerisque sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed risus elit, sagittis sit amet enim ut, maximus posuere turpis. Nam et risus aliquet, bibendum magna eget, rutrum metus. Aliquam blandit tempor mi dictum sagittis. Sed porttitor aliquet finibus. Suspendisse commodo elit non sem varius congue."
 
 @app.route("/first")
 def first():
